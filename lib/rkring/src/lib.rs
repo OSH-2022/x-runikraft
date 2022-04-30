bef8073acfbbef7b515d13c37c37d9dc68191b84
ljw13 <ljw13@mail.ustc.edu.cn>
2022-04-30 21:48:48 +0800

    add rk_ring

--- lib/rkring/src/lib.rs
+++ lib/rkring/src/lib.rs
@@ -3,9 +3,19 @@
 #![no_std]
 
 use rkalloc::*;
+use std::sync::atomic::{AtomicU32, Ordering};
 
 pub struct Ring {
-
+    pub br_prod_head: u32,
+    pub br_prod_tail: u32,
+    pub br_prod_size: u32,
+    pub br_prod_mask: u32,
+    pub br_drops: u64,
+    pub br_cons_head: u32,
+    pub br_cons_tail: u32,
+    pub br_cons_size: u32,
+    pub br_cons_mask: u32,
+    pub br_ring: Vec<*mut u8>,
 }
 
 impl Ring {
@@ -20,29 +30,59 @@ impl Ring {
     pub fn dequeue_mc(&self) -> Option<*mut u8>{
         None
     }
-    pub fn dequeue_sc(&self) -> Option<*mut u8>{
-        None
+    pub fn dequeue_sc(&mut self) -> Option<*mut u8>{
+        let cons_head: u32;
+        let cons_next: u32;
+        let prod_tail: u32;
+        let buf: *mut u8;
+        cons_head = self.br_cons_head;
+        prod_tail = AtomicU32::new(self.br_prod_tail).load(Ordering::SeqCst);
+        cons_next = (cons_head + 1) & self.br_cons_mask as u32;
+        if cons_head == prod_tail {
+            return None
+        }
+        self.br_cons_head = cons_next;
+        buf = self.br_ring[cons_head as usize];
+        self.br_cons_tail = cons_next;
+        return Some(buf)
     }
-    pub fn advance_sc(&self){
-
+    pub fn advance_sc(&mut self){
+        let cons_head = self.br_cons_head;
+        let cons_next = (self.br_cons_head + 1)  & self.br_cons_mask as u32;
+        let prod_tail = self.br_prod_tail;
+        if cons_head == prod_tail {
+            return;
+        }
+        self.br_cons_head = cons_next;
+        self.br_cons_tail = cons_next;
     }
-    pub fn putback_sc(&self, new: *mut u8){
-        
+    pub fn putback_sc(&mut self, new: *mut u8){
+        self.br_ring[self.br_cons_head as usize] = new;
     }
     pub fn peek(&self) -> Option<*mut u8>{
-        None
+        if self.br_cons_head == self.br_prod_tail {
+            None
+        }
+        else {
+            Some(self.br_ring[self.br_cons_head as usize])
+        }
     }
     pub fn peek_clear_sc(&self) -> Option<*mut u8>{
-        None
+        if self.br_cons_head == self.br_prod_tail {
+            None
+        }
+        else {
+            Some(self.br_ring[self.br_cons_head as usize])
+        }
     }
-    pub fn full(&self)->bool{
-        false
+    pub fn full(&self) -> bool{
+        (self.br_prod_head + 1) & self.br_prod_mask == self.br_cons_tail
     }
-    pub fn empty(&self)->bool{
-        false
+    pub fn empty(&self) -> bool{
+        self.br_cons_head == self.br_prod_tail
     }
-    pub fn count(&self)->usize{
-        0
+    pub fn count(&self) -> i32{
+        ((self.br_prod_size + self.br_prod_tail as i32 - self.br_cons_tail as i32) & self.br_prod_mask) as i32
     }
 }
 
---
+++ lib/rkshed/build/rls/debug/deps/rkshed-43d368f8f39350af.d
@@ -0,0 +1,8 @@
+/home/ubuntu/desktop/rustling/x-runikraft/lib/rkshed/build/rls/debug/deps/rkshed-43d368f8f39350af.rmeta: lib/rkshed/src/lib.rs lib/rkshed/src/thread.rs lib/rkshed/src/shed.rs lib/rkshed/src/tests.rs
+
+/home/ubuntu/desktop/rustling/x-runikraft/lib/rkshed/build/rls/debug/deps/rkshed-43d368f8f39350af.d: lib/rkshed/src/lib.rs lib/rkshed/src/thread.rs lib/rkshed/src/shed.rs lib/rkshed/src/tests.rs
+
+lib/rkshed/src/lib.rs:
+lib/rkshed/src/thread.rs:
+lib/rkshed/src/shed.rs:
+lib/rkshed/src/tests.rs:
