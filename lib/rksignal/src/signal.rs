1b0e2b8dc6076a8785477382cd32ab32bc20c794
ljw13 <ljw13@mail.ustc.edu.cn>
2022-05-03 22:36:14 +0800

    define signal.rs struct

---
+++ lib/rksignal/src/signal.rs
@@ -0,0 +1,24 @@
+
+pub struct Thread;
+
+pub struct Signal {
+
+}
+
+pub struct ProcSig {
+
+}
+
+enum RkSigWaiting {
+    SigNotWaiting = 0,
+    SigWaiting = 1,
+    SigWaitingSched = 2,
+}
+
+pub struct ThreadSigWait {
+
+}
+
+pub struct ThreadSig {
+    
+}
