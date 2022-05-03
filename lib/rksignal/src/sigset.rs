4cf21d89fddcd5ea36acf6d858d363bafea486e6
ljw13 <ljw13@mail.ustc.edu.cn>
2022-05-03 22:45:59 +0800

    define sigset.rs function

---
+++ lib/rksignal/src/sigset.rs
@@ -0,0 +1,39 @@
+pub fn sigemptyset(ptr) {
+
+}
+
+pub fn sigfillset(ptr) {
+
+}
+
+pub fn sigaddset(ptr, signo) {
+
+}
+
+pub fn sigdelset(ptr, signo) {
+
+}
+
+pub fn sigcopyset(ptr1, ptr2) {
+
+}
+
+pub fn sigandset(ptr1, ptr2) {
+
+}
+
+pub fn sigorset(ptr1, ptr2) {
+
+}
+
+pub fn sigreverseset(ptr) {
+
+}
+
+pub fn sigismember(ptr, signo) {
+
+}
+
+pub fn sigisempty(ptr) {
+    
+}
