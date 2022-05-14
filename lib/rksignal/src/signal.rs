608cbb47b81896457391dcf53beefba3ff594a75
ljw13 <ljw13@mail.ustc.edu.cn>
2022-05-14 22:41:02 +0800

    define pub struct

--- lib/rksignal/src/signal.rs
+++ lib/rksignal/src/signal.rs
@@ -2,54 +2,54 @@ use runikraft::list::List;
 
 pub type sigset = u64;
 
-union Sahandler {
-    sa_handler: fn(i32),
-    sa_sigaction: fn(i32, *mut Siginfo, *mut u8),
+pub union Sahandler {
+    pub sa_handler: fn(i32),
+    pub sa_sigaction: fn(i32, *mut Siginfo, *mut u8),
 }
 
 pub struct Siginfo {
-    si_signo: i32,
-    si_code: i32,
-    si_pid: i32
+    pub si_signo: i32,
+    pub si_code: i32,
+    pub si_pid: i32
 }
 
 pub struct Sigaction {
-    sa_handler: Sahandler,
-    sa_mask: sigset,
-    sa_flags: i32,
-    sa_restorer: fn()
+    pub sa_handler: Sahandler,
+    pub sa_mask: sigset,
+    pub sa_flags: i32,
+    pub sa_restorer: fn()
 }
 
 pub struct Thread;
 
 pub struct Signal<'a> {
-    info: Siginfo,
-    list_node: List<'a, u8>
+    pub info: Siginfo,
+    pub list_node: List<'a, u8>
 }
 
 pub struct ProcSig<'a> {
-    pending: sigset,
-    pending_signals: [Siginfo; 32],
-    sigaction: [Sigaction; 32],
-    list_node: List<'a, u8>
+    pub pending: sigset,
+    pub pending_signals: [Siginfo; 32],
+    pub sigaction: [Sigaction; 32],
+    pub list_node: List<'a, u8>
 }
 
-enum RkSigWaiting {
+pub enum RkSigWaiting {
     SigNotWaiting = 0,
     SigWaiting = 1,
     SigWaitingSched = 2,
 }
 
 pub struct ThreadSigWait {
-    status: RkSigWaiting,
-    awaited: sigset,
-    received_signal: Siginfo
+    pub status: RkSigWaiting,
+    pub awaited: sigset,
+    pub received_signal: Siginfo
 }
 
 pub struct ThreadSig<'a> {
-    mask: sigset,
-    pending: sigset,
-    pending_signals: List<'a, u8>,
-    wait: ThreadSigWait,
-    list_node: List<'a, u8>
+    pub mask: sigset,
+    pub pending: sigset,
+    pub pending_signals: List<'a, u8>,
+    pub wait: ThreadSigWait,
+    pub list_node: List<'a, u8>
 }
