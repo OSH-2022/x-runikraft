354ebd687814f8267ee7ef9d0743dc7b5a0e94d3
hanhainebula <2512674094@qq.com>
2022-06-26 15:57:30 +0800

    update the alloc_buddy0 test

--- test/alloc_buddy0/src/main.rs
+++ test/alloc_buddy0/src/main.rs
@@ -3,7 +3,6 @@
 // TODO
 #![no_std]
 #![no_main]
-// mod tests{
 extern crate rkalloc;
 extern crate rkalloc_buddy;
 extern crate runikraft;
@@ -22,29 +21,56 @@ static mut HEAP:align_as::A4096<[u8;HEAP_SIZE]> = align_as::A4096::new([0;HEAP_S
 extern "C" fn rkplat_entry(_: i32, _: *mut *mut u8) -> ! {
     let arr_len = 10;
     unsafe {
+        // test `RKallocBuddy::new()` and `RKallocBuddy::alloc()`
         let a = RKallocBuddy::new(HEAP.data.as_mut_ptr(), HEAP.data.len());
         let arr_heap = a.alloc(arr_len*size_of::<usize>(), align_of::<usize>());
+        assert!(!arr_heap.is_null());
         let mut counter: usize = 0;
         let arr = slice::from_raw_parts_mut(arr_heap as *mut i32, arr_len as usize);
-        loop {
-            if counter < arr_len {
-                arr[counter] = (10 - counter) as i32;
-            } else {
-                break;
-            }
+        while counter < arr_len {
+            arr[counter] = (arr_len - counter) as i32;
             counter += 1;
         }
         counter = 0;
-        loop {
-            if counter < arr_len {
-                assert_eq!(arr[counter], (10 - counter) as i32);
-            } else {
-                break;
-            }
+        while counter < arr_len {
+            assert_eq!(arr[counter], (arr_len - counter) as i32);
             counter += 1;
         }
+        
+        // test `RKallocBuddy::realloc()`
+        let new_arr_len = 20;
+        let new_arr_heap = a.realloc(arr_heap, arr_len*size_of::<usize>(), new_arr_len*size_of::<u8>(), align_of::<usize>());
+        assert!(!new_arr_heap.is_null());
+        let new_arr = slice::from_raw_parts_mut(new_arr_heap as *mut u8, new_arr_len as usize);
+        counter = 0;
+        let mut data = 66;
+        while counter < new_arr_len {
+            new_arr[counter] = data;
+            data += 7;
+            counter += 1;
+        }
+        counter = 0;
+        data = 66;
+        while counter < new_arr_len {
+            assert_eq!(new_arr[counter], data);
+            data += 7;
+            counter += 1;
+        }
+
+        // test `RKallocBuddy::dealloc()`
+        a.dealloc(new_arr_heap, new_arr_len*size_of::<u8>(), align_of::<usize>());
+
+        // test 'RKallocBuddy::alloc_zeroed()`
+        let arr_len = 15;
+        let arr_heap = a.alloc_zeroed(arr_len*size_of::<i8>(), align_of::<i8>());
+        let arr = slice::from_raw_parts_mut(arr_heap as *mut i8, arr_len as usize);
+        counter = 0;
+        while counter < arr_len {
+            assert_eq!(arr[counter], 0);
+            counter += 1;
+        }
+        a.dealloc(arr_heap, arr_len*size_of::<i8>(), align_of::<i8>());
     }
-    rkplat::println!("Test alloc_buddy0 passed!");
+    rkplat::println!("\nTest alloc_buddy0 passed!\n");
     rkplat::bootstrap::halt();
 }
-// }
