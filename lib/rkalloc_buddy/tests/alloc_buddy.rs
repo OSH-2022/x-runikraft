9d7148fa17f1363c6dd9a4b2a2982cc12e33a193
hanhainebula <2512674094@qq.com>
2022-06-26 08:39:44 +0800

    Update the test

---
+++ lib/rkalloc_buddy/tests/alloc_buddy.rs
@@ -0,0 +1,48 @@
+// test rkalloc_buddy
+
+// TODO
+#![no_std]
+#![no_main]
+#![feature(default_alloc_error_handler)]
+
+extern crate rkalloc;
+extern crate rkalloc_buddy;
+extern crate runikraft;
+
+use rkalloc::RKalloc;
+use rkalloc_buddy::RKallocBuddy;
+use runikraft::align_as;
+use core::mem::{size_of, align_of};
+use core::slice;
+
+const HEAP_SIZE: usize = 65536;
+
+static mut HEAP:align_as::A4096<[u8;HEAP_SIZE]> = align_as::A4096::new([0;HEAP_SIZE]);
+
+#[test]
+fn alloc_buddy() {
+    let arr_len = 10;
+    unsafe {
+        let a = RKallocBuddy::new(HEAP.data.as_mut_ptr(), HEAP.data.len());
+        let arr_heap = a.alloc(arr_len*size_of::<usize>(), align_of::<usize>());
+        let counter: usize = 0;
+        let arr = slice::from_raw_parts_mut(arr_heap as *mut i32, arr_len as usize);
+        loop {
+            if counter < arr_len {
+                arr[counter] = (10 - counter) as i32;
+            } else {
+                break;
+            }
+            counter += 1;
+        }
+        counter = 0;
+        loop {
+            if counter < arr_len {
+                assert_eq!(arr[counter], (10 - counter) as i32);
+            } else {
+                break;
+            }
+            counter += 1;
+        }
+    }
+}
---
+++ lib/rklist/tests/list.rs
@@ -0,0 +1,3 @@
+// test rklist
+
+// TODO
