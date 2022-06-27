425f08b10f681d2b7de1e3217481874286b75180
hanhainebula <2512674094@qq.com>
2022-06-27 15:16:09 +0800

    complete test_list0

---
+++ test/list0/src/main.rs
@@ -0,0 +1,121 @@
+// test compact_list-list
+
+#![no_std]
+#![no_main]
+
+extern crate rkboot;
+extern crate runikraft;
+extern crate rkalloc;
+
+use runikraft::compat_list::*;
+use rkalloc::*;
+use core::slice;
+use core::mem::{size_of, align_of};
+use core::ptr::NonNull;
+
+#[no_mangle]
+fn main(_args: &mut [&str])->i32 {
+    let mut list_a = List::<i32>::new();
+    unsafe{
+        let a = rkalloc::get_default().expect("error: fail to find global allocator\n");
+        let mut arr_len = 15;
+        let arr_heap = a.alloc(arr_len*size_of::<i32>(), align_of::<i32>());
+        assert!(!arr_heap.is_null());
+        let mut counter: usize = 0;
+        let arr = slice::from_raw_parts_mut(arr_heap as *mut i32, arr_len as usize);
+        while counter < arr_len {
+            arr[counter] = (arr_len - counter) as i32;
+            counter += 1;
+        }
+
+        // test `List::push_front()`, `ListNode::insert_after()`, `ListNode::insert_before()` and `List::iter()`
+        counter = 0;
+        while counter < arr_len {
+            let ptr_e = alloc_type::<ListNode<i32>>(a, ListNode::<i32>::new(arr[counter]));
+            let mut node1 = NonNull::new(ptr_e).expect("error: fail to get node\n");
+            list_a.push_front(node1);
+            counter += 1;
+            if counter < arr_len {
+                let ptr_e = alloc_type::<ListNode<i32>>(a, ListNode::<i32>::new(arr[counter]));
+                let mut node2 = NonNull::new(ptr_e).expect("error: fail to get node\n");
+                node1.as_mut().insert_after(node2);
+                counter += 1;
+                if counter < arr_len {
+                    let ptr_e = alloc_type::<ListNode<i32>>(a, ListNode::<i32>::new(arr[counter]));
+                    let node3 = NonNull::new(ptr_e).expect("error: fail to get node\n");
+                    node2.as_mut().insert_before(node3, Some(&mut list_a));
+                    counter += 1;
+                }
+            }
+        }
+        let list_iter = list_a.iter();
+        let result = [3, 1, 2, 6, 4, 5, 9, 7, 8, 12, 10, 11, 15, 13, 14];
+        counter = 0;
+        for node in list_iter {
+            // rkplat::println!("counter: {}, result in node: {}, expect result: {}", counter, node.as_ref().element, result[counter]);
+            assert_eq!(node.as_ref().element, result[counter]);
+            counter += 1;
+        }
+
+        // test `ListNode::remove()`
+        let list_iter = list_a.iter();
+        let result = [3, 1, 5, 9, 7, 11, 15, 13];
+        counter = 0;
+        for mut node in list_iter {
+            if node.as_ref().element % 2 == 0 {
+                node.as_mut().remove(Some(&mut list_a));
+                a.dealloc(node.as_ptr() as *mut u8, size_of::<ListNode<i32>>(), align_of::<ListNode<i32>>());
+            }
+        }
+        let list_iter = list_a.iter();
+        for node in list_iter {
+            assert_eq!(node.as_ref().element, result[counter]);
+            counter += 1;
+        }
+        arr_len = 8;
+
+        // test 'ListNode::remove_after()` and `ListNode::remove_before()`
+        let list_iter = list_a.iter();
+        let result = [3, 1, 9, 13];
+        counter = 0;
+        for mut node in list_iter {
+            if node.as_ref().element > 10 || node.as_ref().element == 3 {
+                match node.as_mut().remove_before(Some(&mut list_a)) {
+                    None => (),
+                    Some(rm_node) => {
+                        a.dealloc(rm_node.as_ptr() as *mut u8, size_of::<ListNode<i32>>(), align_of::<ListNode<i32>>());
+                    }
+                }
+            }
+            if node.as_ref().element == 1 || node.as_ref().element == 13 {
+                match node.as_mut().remove_after() {
+                    None => (),
+                    Some(rm_node) => {
+                        a.dealloc(rm_node.as_ptr() as *mut u8, size_of::<ListNode<i32>>(), align_of::<ListNode<i32>>());
+                    }
+                }
+            }
+            counter += 1;
+        }
+        let list_iter = list_a.iter();
+        counter = 0;
+        for node in list_iter {
+            assert_eq!(node.as_ref().element, result[counter]);
+            counter += 1;
+        }
+        arr_len = 4;
+
+        // test `List::pop_front()` and `List::is_empty()`
+        counter = 0;
+        while counter < arr_len {
+            let node = list_a.pop_front().expect("error: fail to get node from pop_front()\n");
+            let e = node.as_ref().element;
+            assert_eq!(e, result[counter]);
+            a.dealloc(node.as_ptr() as *mut u8, size_of::<ListNode<i32>>(), align_of::<ListNode<i32>>());
+            counter += 1;
+        }
+        assert!(list_a.is_empty());
+    }
+    rkplat::println!("\nTest list0 passed!\n");
+    return 0;
+}
--- test/tailq0/src/main.rs
+++ test/tailq0/src/main.rs
@@ -39,14 +39,9 @@ extern "C" fn rkplat_entry(_: i32, _: *mut *mut u8) -> ! {
         
         // test `RKallocBuddy::realloc()`
         let new_arr_len = 20;
-        let new_arr_heap = a.realloc(arr_heap, arr_len*size_of::<usize>(), new_arr_len*size_of::<usize>(), align_of::<usize>());
+        let new_arr_heap = a.realloc(arr_heap, arr_len*size_of::<usize>(), new_arr_len*size_of::<u8>(), align_of::<usize>());
         assert!(!new_arr_heap.is_null());
-        let new_arr = slice::from_raw_parts_mut(new_arr_heap as *mut usize, new_arr_len as usize);
-        counter = 0;
-        while counter < arr_len {
-            assert_eq!(arr[counter], (arr_len - counter) as i32);
-            counter += 1;
-        }
+        let new_arr = slice::from_raw_parts_mut(new_arr_heap as *mut u8, new_arr_len as usize);
         counter = 0;
         let mut data = 66;
         while counter < new_arr_len {
@@ -63,7 +58,7 @@ extern "C" fn rkplat_entry(_: i32, _: *mut *mut u8) -> ! {
         }
 
         // test `RKallocBuddy::dealloc()`
-        a.dealloc(new_arr_heap, new_arr_len*size_of::<u8>(), align_of::<u8>());
+        a.dealloc(new_arr_heap, new_arr_len*size_of::<u8>(), align_of::<usize>());
 
         // test 'RKallocBuddy::alloc_zeroed()`
         let arr_len = 15;
