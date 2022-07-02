9e7292a3606610d873521f001931da15e6b89e20
gsxgoldenlegendary <logname@mail.ustc.edu.cn>
2022-07-02 11:00:13 +0800

    trim code in sudoku.

--- example/sudoku/src/main.rs
+++ example/sudoku/src/main.rs
@@ -1,3 +1,38 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// sudoku/main.rs
+
+// Authors:  吴骏东 <1904346407@qq.com>
+// Authors:  郭耸霄 <logname@mail.ustc.edu.cn>
+
+// Copyright (C) 2022 吴骏东, 张子辰, 蓝俊玮, 郭耸霄 and 陈建绿.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+
+
+
+
 /*
     一个数独程序
 
@@ -22,14 +57,13 @@ use core::ptr::null_mut;
 use rklock::*;
 
 
-static mut mutex:Semaphore = Semaphore::new(0);
+static mut mutex: Semaphore = Semaphore::new(0);
 
 pub struct Sudoku {
     // 当前数独信息(玩家显示)
     map: [[usize; 9]; 9],
     // 标记是不是原来的数字。 1 为 原始数字
-    tag: [[usize; 9]; 9]
-
+    tag: [[usize; 9]; 9],
 }
 
 impl Sudoku {
@@ -43,7 +77,7 @@ impl Sudoku {
                     continue;
                 } else {
                     show_sudoku_number(i as u8, j as u8, self.map[i][j] as u8, BLACK);
-                // print!("{} ", self.map[i][j]);
+                    // print!("{} ", self.map[i][j]);
                 }
             }
             // println!("");
@@ -53,12 +87,11 @@ impl Sudoku {
 
 // 数独零初始化
 // 生成一个 9x9 零矩阵
-pub fn sudoku_init_zero () -> Sudoku {
-
+pub fn sudoku_init_zero() -> Sudoku {
     let map_allzero = [[0; 9]; 9];
     let sudoku = Sudoku {
         map: map_allzero,
-        tag: map_allzero
+        tag: map_allzero,
     };
     sudoku
 }
@@ -82,12 +115,11 @@ pub fn row_random(map: &mut [[usize; 9]; 9], row: usize) {
     for i in 0..9 {
         map[row][i] = rowtable[i];
     }
-    
 }
 
 // 数独复制
 // map1 <- map2
-pub fn sudoku_copy(map1: & mut [[usize; 9]; 9], map2: &[[usize; 9]; 9]) {
+pub fn sudoku_copy(map1: &mut [[usize; 9]; 9], map2: &[[usize; 9]; 9]) {
     for i in 0..9 {
         for j in 0..9 {
             map1[i][j] = map2[i][j];
@@ -95,7 +127,7 @@ pub fn sudoku_copy(map1: & mut [[usize; 9]; 9], map2: &[[usize; 9]; 9]) {
     }
 }
 
-pub fn sudoku_setzero(map: & mut [[usize; 9]; 9]) {
+pub fn sudoku_setzero(map: &mut [[usize; 9]; 9]) {
     for i in 0..9 {
         for j in 0..9 {
             map[i][j] = 0;
@@ -108,7 +140,7 @@ pub fn sudoku_setzero(map: & mut [[usize; 9]; 9]) {
  *  如果找到则将结果填入 @nextrow, @nextcol 中，并返回 true
  *  否则返回 false
  */
-pub fn findnext_empty(map: &[[usize; 9]; 9], row: usize, nextrow: & mut usize, nextcol: & mut usize)-> bool {
+pub fn findnext_empty(map: &[[usize; 9]; 9], row: usize, nextrow: &mut usize, nextcol: &mut usize) -> bool {
     for i in row..9 {
         for j in 0..9 {
             if map[i][j] == 0 {
@@ -126,7 +158,7 @@ pub fn findnext_empty(map: &[[usize; 9]; 9], row: usize, nextrow: & mut usize, n
  * 检测内容：在 (@x, @y) 位置填入 @number
  * @if_checkself 为 true 时，如果目标位置存在数字则视作不合法
  */
-pub fn if_fit_check(map: &[[usize; 9]; 9], x: usize, y: usize, number: usize, if_checkself: bool) -> bool{
+pub fn if_fit_check(map: &[[usize; 9]; 9], x: usize, y: usize, number: usize, if_checkself: bool) -> bool {
 
     // 检查目标位置
     if if_checkself && map[x][y] != 0 {
@@ -140,7 +172,7 @@ pub fn if_fit_check(map: &[[usize; 9]; 9], x: usize, y: usize, number: usize, if
         }
         if map[i][y] == number {
             return false;
-        } 
+        }
     }
 
     // 检查列
@@ -150,12 +182,12 @@ pub fn if_fit_check(map: &[[usize; 9]; 9], x: usize, y: usize, number: usize, if
         }
         if map[x][i] == number {
             return false;
-        } 
+        }
     }
 
     // 检查九宫
-    for i in (x/3)*3 .. (x/3)*3 + 3 {
-        for j in (y/3)*3 .. (y/3)*3 + 3 {
+    for i in (x / 3) * 3..(x / 3) * 3 + 3 {
+        for j in (y / 3) * 3..(y / 3) * 3 + 3 {
             if i == x && j == y {
                 continue;
             }
@@ -173,8 +205,7 @@ pub fn if_fit_check(map: &[[usize; 9]; 9], x: usize, y: usize, number: usize, if
  * 返回 true 为有解，返回 false 为无解
  */
 
-pub fn sudoku_solve(map: & mut [[usize; 9]; 9], row: usize, col: usize) -> bool{
-
+pub fn sudoku_solve(map: &mut [[usize; 9]; 9], row: usize, col: usize) -> bool {
     let mut nextrow: usize = 0;
     let mut nextcol: usize = 0;
 
@@ -191,7 +222,7 @@ pub fn sudoku_solve(map: & mut [[usize; 9]; 9], row: usize, col: usize) -> bool{
 
         map[row][col] = number;
 
-        if !(findnext_empty(map, row, & mut nextrow, & mut nextcol)) {
+        if !(findnext_empty(map, row, &mut nextrow, &mut nextcol)) {
             // 没有空位了，数独求解完成 
             return true;
         }
@@ -199,8 +230,7 @@ pub fn sudoku_solve(map: & mut [[usize; 9]; 9], row: usize, col: usize) -> bool{
         if !(sudoku_solve(map, nextrow, nextcol)) {
             map[row][col] = 0;
             continue;
-        }
-        else {
+        } else {
             return true;
         }
     }
@@ -212,7 +242,7 @@ pub fn sudoku_solve(map: & mut [[usize; 9]; 9], row: usize, col: usize) -> bool{
     以 @map 为模板，将挖空的结果写入 @map
     @num 为留下的非空格数字数目，最低为 10
 */
-pub fn hole_dig(map:& mut [[usize; 9]; 9], num: usize, tag: &mut [[usize; 9]; 9]) {
+pub fn hole_dig(map: &mut [[usize; 9]; 9], num: usize, tag: &mut [[usize; 9]; 9]) {
     let mut hole_map = [[0; 9]; 9];
 
     let mut number_num = num % 81;
@@ -226,14 +256,14 @@ pub fn hole_dig(map:& mut [[usize; 9]; 9], num: usize, tag: &mut [[usize; 9]; 9]
         // let mut index = rng.gen_range(0..81);
         // let time = wall_clock();
         // let mut index = (time.as_nanos() % 81) as usize;
-        let mut index:usize = fast_random::<usize>() % 81;
+        let mut index: usize = fast_random::<usize>() % 81;
         loop {
             if index >= 81 {
                 index %= 81;
             }
             if hole_map[index / 9][index % 9] != 0 {
                 index += 1;
-                continue
+                continue;
             }
             hole_map[index / 9][index % 9] = map[index / 9][index % 9];
             tag[index / 9][index % 9] = 1;
@@ -242,8 +272,7 @@ pub fn hole_dig(map:& mut [[usize; 9]; 9], num: usize, tag: &mut [[usize; 9]; 9]
         i += 1;
     }
 
-    sudoku_copy(map, & hole_map);
-
+    sudoku_copy(map, &hole_map);
 }
 
 /*
@@ -252,21 +281,21 @@ pub fn hole_dig(map:& mut [[usize; 9]; 9], num: usize, tag: &mut [[usize; 9]; 9]
     如果该位置原先有数字，则写入失败
     @ifcheck 为 true 时，如果填入的数字破坏了数独规则也写入失败
 */
-pub fn add_num(map: &mut [[usize; 9]; 9], row:usize, col:usize, num: usize, ifcheck:bool) -> bool {
+pub fn add_num(map: &mut [[usize; 9]; 9], row: usize, col: usize, num: usize, ifcheck: bool) -> bool {
     if row > 8 || col > 8 {
         // 访问越界
         return false;
     }
     if map[row][col] != 0 {
-        unsafe{mutex.signal();}
+        unsafe { mutex.signal(); }
         return false;
     }
-    if num > 9 || num <= 0{
-        unsafe{mutex.signal();}
+    if num > 9 || num <= 0 {
+        unsafe { mutex.signal(); }
         return false;
     }
     if ifcheck && !(if_fit_check(map, row, col, num, false)) {
-        unsafe{mutex.signal();}
+        unsafe { mutex.signal(); }
         return false;
     }
 
@@ -279,8 +308,8 @@ pub fn add_num(map: &mut [[usize; 9]; 9], row:usize, col:usize, num: usize, ifch
     将 @map 中 (@row, @col) 的位置上数字删除（写入 0）
     如果该位置原先就是 0，则删除失败
 */
-pub fn del_num(map: &mut [[usize; 9]; 9], tag: &[[usize; 9]; 9], row:usize, col:usize) -> bool {
-    if row > 8 || col > 8  {
+pub fn del_num(map: &mut [[usize; 9]; 9], tag: &[[usize; 9]; 9], row: usize, col: usize) -> bool {
+    if row > 8 || col > 8 {
         // 访问越界
         return false;
     }
@@ -300,22 +329,22 @@ pub fn del_num(map: &mut [[usize; 9]; 9], tag: &[[usize; 9]; 9], row:usize, col:
 /*
     提示函数：该函数将根据当前 @map 的内容，求解数独，并将解填入第一个空格。
  */
-pub fn hint(map: &mut [[usize; 9]; 9]) -> bool{
-    let mut map_allzero:[[usize; 9]; 9] = [[0; 9]; 9];
-    sudoku_copy(& mut map_allzero, map);
+pub fn hint(map: &mut [[usize; 9]; 9]) -> bool {
+    let mut map_allzero: [[usize; 9]; 9] = [[0; 9]; 9];
+    sudoku_copy(&mut map_allzero, map);
 
     let mut nextrow: usize = 0;
     let mut nextcol: usize = 0;
 
-    if !(findnext_empty(map, 0, & mut nextrow, & mut nextcol)) {
+    if !(findnext_empty(map, 0, &mut nextrow, &mut nextcol)) {
         // 没有空位了 
         return false;
     }
-    sudoku_solve(& mut map_allzero, nextrow, nextcol);
+    sudoku_solve(&mut map_allzero, nextrow, nextcol);
 
     add_num(map, nextrow, nextcol, map_allzero[nextrow][nextcol], false);
 
-    unsafe{show_sudoku_number(nextrow as u8, nextcol as u8, map_allzero[nextrow][nextcol] as u8, GOLD);}
+    unsafe { show_sudoku_number(nextrow as u8, nextcol as u8, map_allzero[nextrow][nextcol] as u8, GOLD); }
 
     return true;
 }
@@ -333,10 +362,7 @@ pub fn error_hinter(_null: *mut u8) {
 
 #[no_mangle]
 fn main() {
-
-
     unsafe {
-
         sched::create_thread("", rkalloc::get_default().unwrap(),
                              rksched::thread::ThreadAttr::default(), rksched::thread::ThreadLimit::default(),
                              input_tracer, null_mut()).expect("TODO: panic message");
@@ -349,54 +375,51 @@ fn main() {
         let mut map_old: [[usize; 9]; 9] = [[0; 9]; 9];
 
         init();
-        printg("Use W, A, S, and D to move selecting rectangle.\nUse up, left, down, and right to move cursor.\nUse H for hint, use O for solution.",0,700,BLACK,255,2);
+        printg("Use W, A, S, and D to move selecting rectangle.\nUse up, left, down, and right to move cursor.\nUse H for hint, use O for solution.", 0, 700, BLACK, 255, 2);
         draw_sudoku_lattices(PURPLE, BLACK);
         screen_flush();
         row_random(&mut sudoku.map, 0);
         sudoku_solve(&mut sudoku.map, 1, 1);
 
-        hole_dig(&mut sudoku.map, 15,&mut sudoku.tag);
+        hole_dig(&mut sudoku.map, 15, &mut sudoku.tag);
         sudoku.map_print();
 
-    sudoku_copy(& mut map_old, & sudoku.map);
-    
-    
+        sudoku_copy(&mut map_old, &sudoku.map);
 
-    
-    
-    loop {
-        rksched::this_thread::sleep_for(Duration::from_millis(1));
 
-        if INPUT_NUMBER >= 1 && INPUT_NUMBER <= 9 && add_num(&mut sudoku.map, SELECT_X as usize / 75 , SELECT_Y as usize / 75, INPUT_NUMBER, true) {
-        //if add_num(&mut sudoku.map, 0 , 0, INPUT_NUMBER, true) {
-            show_sudoku_number((SELECT_X / 75) as u8, (SELECT_Y / 75) as u8, (INPUT_NUMBER) as u8, GRAY);
-            //show_sudoku_number(0, 0, INPUT_NUMBER as u8, GRAY);
+        loop {
+            rksched::this_thread::sleep_for(Duration::from_millis(1));
 
+            if INPUT_NUMBER >= 1 && INPUT_NUMBER <= 9 && add_num(&mut sudoku.map, SELECT_X as usize / 75, SELECT_Y as usize / 75, INPUT_NUMBER, true) {
+                //if add_num(&mut sudoku.map, 0 , 0, INPUT_NUMBER, true) {
+                show_sudoku_number((SELECT_X / 75) as u8, (SELECT_Y / 75) as u8, (INPUT_NUMBER) as u8, GRAY);
+                //show_sudoku_number(0, 0, INPUT_NUMBER as u8, GRAY);
+            }
+
+            if INPUT_NUMBER == 0 && del_num(&mut sudoku.map, &sudoku.tag, SELECT_X as usize / 75, SELECT_Y as usize / 75) {
+                show_sudoku_number((SELECT_X / 75) as u8, (SELECT_Y / 75) as u8, 0, GRAY);
+                //show_sudoku_number(SELECT_X as u8 / 75, SELECT_Y as u8 / 75, 255, BLACK);
+            }
+
+            if INPUT_NUMBER == 35 {
+                hint(&mut &mut sudoku.map);
+            }
+
+            if INPUT_NUMBER == 24 {
+                sudoku_solve(&mut sudoku.map, 0, 0);
+                sudoku.map_print();
+            }
+            INPUT_NUMBER = 200;
         }
-
-        if INPUT_NUMBER == 0 && del_num(&mut sudoku.map, &sudoku.tag, SELECT_X as usize / 75, SELECT_Y as usize / 75) {
-            show_sudoku_number((SELECT_X / 75) as u8, (SELECT_Y / 75) as u8, 0, GRAY);
-            //show_sudoku_number(SELECT_X as u8 / 75, SELECT_Y as u8 / 75, 255, BLACK);
-        }
-
-        if INPUT_NUMBER == 35 {
-            hint(&mut &mut sudoku.map);
-        }
-
-        if INPUT_NUMBER == 24 {
-            sudoku_solve(& mut sudoku.map, 0, 0);
-            sudoku.map_print();
-        }
-        INPUT_NUMBER = 200;
-    }
-    // sudoku_solve(& mut sudoku.map, & mut sudoku.answer, 0, 0);
-    // unsafe { sudoku.map_print(); }
+        // sudoku_solve(& mut sudoku.map, & mut sudoku.answer, 0, 0);
+        // unsafe { sudoku.map_print(); }
     }
 }
 
 use rkplat::drivers::virtio::GPU_DEIVCE;
-use rkgpu::{draw_font,DIRECTION,draw_line};
-unsafe fn draw_sudoku_lattices(color0:Color,color1:Color) -> u8 {
+use rkgpu::{draw_font, DIRECTION, draw_line};
+
+unsafe fn draw_sudoku_lattices(color0: Color, color1: Color) -> u8 {
     let (width, height) = GPU_DEIVCE.as_mut().unwrap().resolution();
     if width >= 750 && height >= 750 {
         for x in 0..10 {
@@ -417,14 +440,14 @@ unsafe fn draw_sudoku_lattices(color0:Color,color1:Color) -> u8 {
     } else { 0 }
 }
 
-unsafe fn show_sudoku_number(pos_x: u8, pos_y: u8, number: u8,color:Color) -> u8 {
+unsafe fn show_sudoku_number(pos_x: u8, pos_y: u8, number: u8, color: Color) -> u8 {
     if pos_x <= 8 && pos_y <= 8 {
         let start_x: u32 = 75 * pos_x as u32 + 20;
         let start_y: u32 = 75 * pos_y as u32 + 6;
         if number == 0 {
-            draw_font(start_x, start_y, CYAN, 255,' ', 4);
+            draw_font(start_x, start_y, CYAN, 255, ' ', 4);
         } else {
-            draw_font(start_x, start_y, color, 255,(number + 48).into(), 4);
+            draw_font(start_x, start_y, color, 255, (number + 48).into(), 4);
         }
         screen_flush();
         0
