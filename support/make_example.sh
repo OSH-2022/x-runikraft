98f7f6decc60a4b60318aa7183091b402000b2a3
hanhainebula <2512674094@qq.com>
2022-07-03 08:53:53 +0800

    modify some errors in make_example.sh

--- support/make_example.sh
+++ support/make_example.sh
@@ -3,7 +3,7 @@ make_type=`cat ${CONFIG_DIR}/.features2`
 features=`cat ${CONFIG_DIR}/.features1`
 if [ $make_type = "release" ]
 then
-    cd example/sudoku && env RUNIKRAFT_CONFIG_FILE="${CONFIG_DIR}/config.rs" RUSTFLAGS="-Clink-arg=-T${SRC_ROOT_DIR}/linker.ld --cfg __alloc_error_handler --cfg __runikraft_custom_config--extern __alloc_error_handler=${MAKE_ROOT_DIR}/liballoc_error_handler.rlib" cargo build --release $features
+    cd example/sudoku && env RUNIKRAFT_CONFIG_FILE="${CONFIG_DIR}/config.rs" RUSTFLAGS="-Clink-arg=-T${SRC_ROOT_DIR}/linker.ld --cfg __alloc_error_handler --cfg __runikraft_custom_config --extern __alloc_error_handler=${MAKE_ROOT_DIR}/liballoc_error_handler.rlib" cargo build --release $features
 elif [ $make_type = "debug" ]
 then
     cd example/sudoku && env RUNIKRAFT_CONFIG_FILE="${CONFIG_DIR}/config.rs" RUSTFLAGS="-Clink-arg=-T${SRC_ROOT_DIR}/linker.ld --cfg __alloc_error_handler --cfg __runikraft_custom_config --extern __alloc_error_handler=${MAKE_ROOT_DIR}/liballoc_error_handler.rlib" cargo build $features
